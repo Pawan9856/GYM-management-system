06984278d324701fe4d05dc260c3658a5d1426e9
Pawan9856 <pawanpaliwal4444@gmail.com>
2024-06-14 21:04:30 +0530

    creating react App and making components and page directories

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1>pawan is here!!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { Login } from "./pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
